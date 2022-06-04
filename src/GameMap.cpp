e07e285a5709018d2319e89347a081fed8eff430
timre13 <itorteli13@gmail.com>
2022-06-04 19:13:10 +0200

    GameMap: Load basic map info from file

--- src/GameMap.cpp
+++ src/GameMap.cpp
@@ -1,9 +1,173 @@
 #include "GameMap.h"
 #include "Logger.h"
+#include <cjson/cJSON.h>
+#include <fstream>
+#include <sstream>
 
-GameMap::GameMap()
+#define MAP_KEY_NAME                "name"
+#define MAP_KEY_DESCR               "descr"
+#define MAP_KEY_AUTHOR              "author"
+#define MAP_KEY_MAP_FORM_VER        "mapFormatVer"
+#define MAP_KEY_MAP_FORM_VER_MAJ    "major"
+#define MAP_KEY_MAP_FORM_VER_MIN    "minor"
+#define MAP_KEY_OBJS                "objects"
+
+enum class JsonType
 {
-#if 1 // ----- TEST -----
+    Bool,
+    Null,
+    Number,
+    String,
+    Array,
+    Object,
+    Raw,
+};
+
+template <JsonType TypeVal>
+static void checkItemType(const cJSON* item)
+{
+    static constexpr const char* typeNames[] = {
+        "Bool",
+        "Null",
+        "Number",
+        "String",
+        "Array",
+        "Object",
+        "Raw",
+    };
+
+    if constexpr (TypeVal == JsonType::Bool)
+    {
+        if (cJSON_IsBool(item)) return;
+    }
+    else if constexpr (TypeVal == JsonType::Null)
+    {
+        if (cJSON_IsNull(item)) return;
+    }
+    else if constexpr (TypeVal == JsonType::Number)
+    {
+        if (cJSON_IsNumber(item)) return;
+    }
+    else if constexpr (TypeVal == JsonType::String)
+    {
+        if (cJSON_IsString(item)) return;
+    }
+    else if constexpr (TypeVal == JsonType::Array)
+    {
+        if (cJSON_IsArray(item)) return;
+    }
+    else if constexpr (TypeVal == JsonType::Object)
+    {
+        if (cJSON_IsObject(item)) return;
+    }
+    else if constexpr (TypeVal == JsonType::Raw)
+    {
+        if (cJSON_IsRaw(item)) return;
+    }
+    else
+    {
+        assert(0 && "Tried to check for invalid JSON type");
+        abort();
+    }
+
+    const std::string itemStr = (item ? cJSON_Print(item) : "<nullptr>");
+    throw std::runtime_error(std::string("Expected JSON item of type ")
+            +typeNames[(int)TypeVal]+", but got something else: \'"
+            +itemStr+'\'');
+}
+
+static GameMap::MapFormatVer createMapFormatVer(const cJSON* item)
+{
+    checkItemType<JsonType::Object>(item);
+
+    auto isWhole{[](double x){ // -> bool
+        return round(x) == x;
+    }};
+
+    uint major{};
+    {
+        const cJSON* majorJson = cJSON_GetObjectItem(item, MAP_KEY_MAP_FORM_VER_MAJ);
+        checkItemType<JsonType::Number>(majorJson);
+        const double num = cJSON_GetNumberValue(majorJson);
+        if (!isWhole(num))
+        {
+            throw std::runtime_error{MAP_KEY_MAP_FORM_VER ":" MAP_KEY_MAP_FORM_VER_MAJ
+                ": Expected value of type int, got float"};
+        }
+        major = num;
+    }
+
+    uint minor{};
+    {
+        const cJSON* minorJson = cJSON_GetObjectItem(item, MAP_KEY_MAP_FORM_VER_MIN);
+        checkItemType<JsonType::Number>(minorJson);
+        const double num = cJSON_GetNumberValue(minorJson);
+        if (!isWhole(num))
+        {
+            throw std::runtime_error{MAP_KEY_MAP_FORM_VER ":" MAP_KEY_MAP_FORM_VER_MIN
+                ": Expected value of type int, got float"};
+        }
+        minor = num;
+    }
+
+    return {.major = major, .minor = minor};
+
+}
+
+static btCollisionShape* createCollShape(const cJSON* item)
+{
+    return nullptr; // TODO
+}
+
+static glm::vec3 createVec3(const cJSON* item)
+{
+    return {}; // TODO
+}
+
+static std::string readFile(const std::string& path)
+{
+    std::fstream file;
+    file.open(path, std::ios_base::in);
+    if (!file.is_open())
+        assert(false); // TODO
+
+    std::stringstream ss;
+    ss << file.rdbuf();
+    file.close();
+    return ss.str();
+}
+
+static void printParseErr(const std::string& content, size_t errPos)
+{
+    assert(errPos < content.length());
+
+    std::stringstream ss;
+    ss << content;
+    size_t pos{};
+    size_t lineI{};
+    std::string line;
+    while (std::getline(ss, line))
+    {
+        if (errPos >= pos && errPos < pos+line.length()+1)
+        {
+            Logger::err << "Error in map JSON: in line " << lineI+1 << ", col " << errPos-pos+1 << "\n"
+                << "\t: " << line << "\n"
+                << "\t: " << std::string(errPos-pos, ' ') << "^" << Logger::End;
+            break;
+        }
+        pos += line.length()+1;
+        ++lineI;
+    }
+}
+
+static std::string mapFormatVerToStr(const GameMap::MapFormatVer& val)
+{
+    return std::to_string(val.major) + '.' + std::to_string(val.minor);
+}
+
+GameMap::GameMap(const std::string& path)
+{
+#if 0 // ----- TEST -----
     static constexpr float floorScale = 20.0f;
     //int x = 0;
     //int y = 0;
@@ -123,55 +287,81 @@ GameMap::GameMap()
             .mass = 10.0f,
     }});
 
-
-    //m_objects.push_back(std::unique_ptr<ObjectDescr>{new ObjectDescr{
-    //        .objName = "Cube",
-    //        .modelName = "cube.obj",
-    //        .textureName = "",
-    //        .pos = {
-    //            1.0f,
-    //            0.1f,
-    //            1.0f,
-    //        },
-    //        .scale = {
-    //            1.0f,
-    //            1.0f,
-    //            1.0f,
-    //        },
-    //}});
-
-    //m_objects.push_back(std::unique_ptr<ObjectDescr>{new ObjectDescr{
-    //        .objName = "Bucket",
-    //        .modelName = "bucket.obj",
-    //        .textureName = "bucket/Bucket3Albedo.png",
-    //        .pos = {
-    //            1.5f,
-    //            0.0f,
-    //            -3.0f,
-    //        },
-    //        .scale = {
-    //            1.0f,
-    //            1.0f,
-    //            1.0f,
-    //        },
-    //}});
-
-    //m_objects.push_back(std::unique_ptr<ObjectDescr>{new ObjectDescr{
-    //        .objName = "Quake Guy",
-    //        .modelName = "private/player.obj",
-    //        .textureName = "private/player_0.png",
-    //        .pos = {
-    //            -3.0f,
-    //            1.2f,
-    //            0.0f,
-    //        },
-    //        .scale = {
-    //            0.05f,
-    //            0.05f,
-    //            0.05f,
-    //        },
-    //}});
+    m_objects.push_back(std::unique_ptr<ObjectDescr>{new ObjectDescr{
+            .objName = "Quake Guy",
+            .modelName = "private/player.obj",
+            .textureName = "private/player_0.png",
+            .pos = {
+                2.0f,
+                11.0f,
+                0.0f,
+            },
+            .scale = {
+                0.05f,
+                0.05f,
+                0.05f,
+            },
+            .collShape{new btBoxShape{{0.6f, 1.2f, 0.4f}}},
+            .mass=1.0f,
+    }});
 #endif
 
-    Logger::log << "Loaded map with " << m_objects.size() << " objects" << Logger::End;
+    Logger::log << "Loading map: \"" << path << '"' << Logger::End;
+
+    const std::string fileContent = readFile(path);
+    const char* fileContentPtr = fileContent.c_str();
+
+    cJSON* json = cJSON_Parse(fileContentPtr);
+    if (!json)
+    {
+        printParseErr(fileContent, cJSON_GetErrorPtr()-fileContentPtr);
+        throw std::runtime_error{"Error in map file"};
+    }
+
+    if (!cJSON_IsObject(json))
+    {
+        Logger::err << "Root is not an Object" << Logger::End;
+        throw std::runtime_error{"Error in map file"};
+    }
+
+    {
+        const cJSON* nameJson = cJSON_GetObjectItem(json, MAP_KEY_NAME);
+        checkItemType<JsonType::String>(nameJson);
+        m_name = cJSON_GetStringValue(nameJson);
+    }
+
+    if (const cJSON* descrJson = cJSON_GetObjectItem(json, MAP_KEY_DESCR)) // Don't check if not given
+    {
+        checkItemType<JsonType::String>(descrJson);
+        m_descr = cJSON_GetStringValue(descrJson);
+    }
+
+    if (const cJSON* authorJson = cJSON_GetObjectItem(json, MAP_KEY_AUTHOR)) // Don't check if not given
+    {
+        checkItemType<JsonType::String>(authorJson);
+        m_author = cJSON_GetStringValue(authorJson);
+    }
+
+    {
+        const cJSON* formatVerJson = cJSON_GetObjectItem(json, MAP_KEY_MAP_FORM_VER);
+        checkItemType<JsonType::Object>(formatVerJson);
+        m_mapFormatVer = createMapFormatVer(formatVerJson);
+    }
+
+    const cJSON* objs = cJSON_GetObjectItem(json, MAP_KEY_OBJS);
+    checkItemType<JsonType::Array>(objs);
+    Logger::verb << "\"" MAP_KEY_OBJS "\" array length: "+std::to_string(cJSON_GetArraySize(objs)) << Logger::End;
+    const cJSON* obj{};
+    cJSON_ArrayForEach(obj, objs)
+    {
+    }
+
+    Logger::log << "Loaded map" << Logger::End;
+    Logger::verb << "----- Map info -----" << '\n';
+    Logger::verb << '\n' << "Name: " << m_name;
+    Logger::verb << '\n' << "Description: " << m_descr;
+    Logger::verb << '\n' << "Author: " << m_author;
+    Logger::verb << '\n' << "Map format version: " << mapFormatVerToStr(m_mapFormatVer);
+    Logger::verb << '\n' << "Number of objects: " << m_objects.size();
+    Logger::verb << Logger::End;
 }
--- src/GameMap.h
+++ src/GameMap.h
@@ -9,27 +9,40 @@
 
 class GameMap final
 {
-private:
+public:
+    struct MapFormatVer
+    {
+        uint major;
+        uint minor;
+    };
+
     struct ObjectDescr
     {
-        std::string             objName;
-        std::string             modelName;
-        std::string             textureName;
+        std::string             objName = "<Object>";
+        std::string             modelName; // Required
+        std::string             textureName; // Required
         GameObject::flag_t      flags = GameObject::defaultFlags;
 
-        glm::vec3               pos{0.0f, 0.0f, 0.0f};
+        glm::vec3               pos{}; // Required
         glm::vec3               scale{1.0f, 1.0f, 1.0f};
         glm::vec3               modelRotDeg{0.0f, 0.0f, 0.0f};
 
         std::unique_ptr<btCollisionShape> collShape;
-        btScalar                mass = 1.0f;
+        btScalar                mass = 0.0f;
     };
-
     using objectList_t = std::vector<std::unique_ptr<ObjectDescr>>;
+
+private:
+    std::string m_name; // Required
+    std::string m_descr = "N/A";
+    std::string m_author = "<unknown>";
+
+    MapFormatVer m_mapFormatVer;
+
     objectList_t m_objects;
 
 public:
-    GameMap();
+    GameMap(const std::string& path);
 
     inline const objectList_t& getObjects() const { return m_objects; }
 };
